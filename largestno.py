097ef348ff7584bda3137b2b93a3264932eacf44
vaishudurai <46399268+vaishudurai@users.noreply.github.com>
2019-01-09 21:09:08 +0530

    Update largestno.py

--- largestno.py
+++ largestno.py
@@ -1,9 +1,9 @@
 num1=input()
 num2=input()
 num3=input()
-if (num1>num2) and (num1>num3):
+if (num1 > num2) and (num1 > num3):
    largest=num1
-elif (num2>num1) and (num2>num3):   
+elif (num2 > num1) and (num2 > num3):   
    largest=num2
 else :
    largest=num3
