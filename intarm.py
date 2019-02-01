875e819ad69063dee12d8688bf83e88ac712bb34
vaishudurai <46399268+vaishudurai@users.noreply.github.com>
2019-02-01 10:20:51 +0600

    Create intarm.py

---
+++ intarm.py
@@ -0,0 +1,12 @@
+upper,lower=map(int,raw_input().split())
+for num in range(upper+1,lower ):
+   order = len(str(num))
+   sum = 0
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if num == sum:
+       print(num)
