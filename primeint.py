f9399f9f2d6c3a5fa5fdf58032e7bfc774505613
vaishudurai <46399268+vaishudurai@users.noreply.github.com>
2019-02-17 18:27:35 +0530

    Update primeint.py

--- primeint.py
+++ primeint.py
@@ -1,8 +1,10 @@
 l,r=map(int,raw_input().split())
-for num in range(l +1,r):
+c=0
+for num in range(l,r+1):
    if num > 1:
        for i in range(2,num):
            if (num % i) == 0:
                break
        else:
-           print(num)
+           c+=1
+print(c)
